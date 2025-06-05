d6238516af4b12f8f896a4da03e31202f2a1ce7d
David Kesoshvili <david.kesoshvili@checkmarx.com>
2025-06-05 14:28:48 +0300

    Update files_extractor_test.go

--- pkg/imagesExtractor/files_extractor_test.go
+++ pkg/imagesExtractor/files_extractor_test.go
@@ -11,7 +11,6 @@ import (
 
 func TestExtractAndMergeImagesFromFiles(t *testing.T) {
 	extractor := NewImagesExtractor()
-
 	// Define test scenarios
 	scenarios := []struct {
 		Name             string
